542ba1d3634ca730c36f160d2a9914a58d812c17
Paul Lindberg <paul.lindberg@mymail.champlain.edu>
2018-04-06 16:32:18 -0400

    Moved stuff to twitter_sql_insert

--- twitter_sql_functions.sql
+++ twitter_sql_functions.sql
@@ -1,17 +1,3 @@
-#Allow users to post short messages (the length limit is up to you)
-INSERT INTO `mydb`.`tweets`
-(`Tweet_PrimaryKey`,`Content`,`Date`,`Users_idUsers`)
-VALUES
-(01,"Hello World! This is our first post!",NOW(),1);
-INSERT INTO `mydb`.`tweets`
-(`Tweet_PrimaryKey`,`Content`,`Date`,`Users_idUsers`)
-VALUES
-(02,"Ayyyyyyyy another post!",NOW(),1);
-INSERT INTO `mydb`.`tweets`
-(`Tweet_PrimaryKey`,`Content`,`Date`,`Users_idUsers`)
-VALUES
-(03,"ffkjhalkfj Oops better delete this tweet!",NOW(),1);
-
 
 /*
 # Return tweet and possible retweet data for a specific tweet
@@ -23,16 +9,6 @@ LEFT OUTER JOIN retweet AS rt ON rt.Tweets_Tweet_PrimaryKey = t.Tweet_PrimaryKey
 WHERE
     t.Tweet_PrimaryKey = 01;
 */
-
-
-#Follow/unfollow other users
-# Super Ninja Fat follows TheAllMightyRa
-INSERT INTO `mydb`.`following`
-(`Users_idUsers_followed`,
-`Users_idUsers_follower`)
-VALUES
-(2,
-1);
 
 
 #see a user’s timeline (in chronological order - the timeline is the combination of tweets/retweets of all users that one follows)
@@ -53,53 +29,6 @@ a order by `Date` desc;
 # Same code but focusing on TheAllMightyRa (He follows Super Ninja Fat)
 
 
-#Retweet
-# Super Ninja Fat retweets his own first tweet
-INSERT INTO `mydb`.`retweet`
-(`Users_idUsers`,`Tweets_Tweet_PrimaryKey`,`Date`)
-VALUES
-(1,01,NOW());
-# TheAllMightyRa retweets Super Ninja Fat's first tweet
-INSERT INTO `mydb`.`retweet`
-(`Users_idUsers`,`Tweets_Tweet_PrimaryKey`,`Date`)
-VALUES
-(2,01,NOW());
-# Super Ninja Fat retweets his own second tweet
-INSERT INTO `mydb`.`retweet`
-(`Users_idUsers`,`Tweets_Tweet_PrimaryKey`,`Date`)
-VALUES
-(1,02,NOW());
-
-#Delete a tweet
-# Delete Super Ninja Fat's third tweet
-DELETE FROM tweets
-WHERE Tweet_PrimaryKey = 3;
-
-#Favorite/unfavorite tweets
-INSERT INTO `mydb`.`favorites`
-(`idFavorites`,
-`Users_idUsers`,
-`Tweets_Tweet_PrimaryKey`)
-VALUES
-(01,
-1,
-02);
-INSERT INTO `mydb`.`favorites`
-(`idFavorites`,
-`Users_idUsers`,
-`Tweets_Tweet_PrimaryKey`)
-VALUES
-(02,
-1,
-01);
-INSERT INTO `mydb`.`favorites`
-(`idFavorites`,
-`Users_idUsers`,
-`Tweets_Tweet_PrimaryKey`)
-VALUES
-(04,
-2,
-01);
 
 #See a user’s favorited tweets
 /*SELECT u.Username, t.Content, t.`Date` from tweets as t
--- twitter_sql_insert.sql
+++ twitter_sql_insert.sql
@@ -1,9 +1,77 @@
+DELETE FROM favorites;
+DELETE FROM following;
+DELETE FROM retweet;
+DELETE FROM tweets;
+DELETE FROM users;
+
+# Creating users
 INSERT INTO `mydb`.`users`
 (`idUsers`, `Username`)
 VALUES
 (1, 'Super Ninja Fat');
-
 INSERT INTO `mydb`.`users`
 (`idUsers`, `Username`)
 VALUES
 (2, 'TheAllMightyRa');
+
+#Allow users to post short messages (the length limit is up to you)
+INSERT INTO `mydb`.`tweets`
+(`Tweet_PrimaryKey`,`Content`,`Date`,`Users_idUsers`)
+VALUES
+(01,"Hello World! This is our first post!",NOW(),1);
+INSERT INTO `mydb`.`tweets`
+(`Tweet_PrimaryKey`,`Content`,`Date`,`Users_idUsers`)
+VALUES
+(02,"Ayyyyyyyy another post!",NOW(),1);
+INSERT INTO `mydb`.`tweets`
+(`Tweet_PrimaryKey`,`Content`,`Date`,`Users_idUsers`)
+VALUES
+(03,"ffkjhalkfj Oops better delete this tweet!",NOW(),1);
+
+#Follow/unfollow other users
+# Super Ninja Fat follows TheAllMightyRa
+INSERT INTO `mydb`.`following`
+(`Users_idUsers_followed`,
+`Users_idUsers_follower`)
+VALUES
+(2,
+1);
+
+#Retweet
+# Super Ninja Fat retweets his own first tweet
+INSERT INTO `mydb`.`retweet`
+(`Users_idUsers`,`Tweets_Tweet_PrimaryKey`,`Date`)
+VALUES
+(1,01,NOW());
+# TheAllMightyRa retweets Super Ninja Fat's first tweet
+INSERT INTO `mydb`.`retweet`
+(`Users_idUsers`,`Tweets_Tweet_PrimaryKey`,`Date`)
+VALUES
+(2,01,NOW());
+# Super Ninja Fat retweets his own second tweet
+INSERT INTO `mydb`.`retweet`
+(`Users_idUsers`,`Tweets_Tweet_PrimaryKey`,`Date`)
+VALUES
+(1,02,NOW());
+
+#Delete a tweet
+# Delete Super Ninja Fat's third tweet
+DELETE FROM tweets
+WHERE Tweet_PrimaryKey = 3;
+
+#Favorite/unfavorite tweets
+INSERT INTO `mydb`.`favorites`
+(`idFavorites`,`Users_idUsers`,`Tweets_Tweet_PrimaryKey`)
+VALUES
+(01,1,02);
+INSERT INTO `mydb`.`favorites`
+(`idFavorites`,`Users_idUsers`,`Tweets_Tweet_PrimaryKey`)
+VALUES
+(02,1,01);
+INSERT INTO `mydb`.`favorites`
+(`idFavorites`,
+`Users_idUsers`,
+`Tweets_Tweet_PrimaryKey`)
+VALUES
+(04,2,01);
+
